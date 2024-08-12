4976cf34ab5ab190648d24fa6720b754c034ba15
[jehiguera] <[jhiguera@yahoo.com]>
2024-08-12 19:12:59 +0200

    Updated webpage

--- webscraping_journal_page.py
+++ webscraping_journal_page.py
@@ -1,16 +1,16 @@
-#Ejemplo de web scrapping en la web de www.elpais para descargar noticias que no se pueden leer sin estar con la suscripcion de pago
+#Example to do web scrapping on the web of an online news newspaper
 #Jhiguera@ieee.org
-#06/08/2022
+#12/08/2024
 
 from http.cookies import _quote
 from smtplib import quotedata
 import requests
 from bs4 import BeautifulSoup
-ficheroNoticia2 = open('Elpais3.txt','w', encoding='utf-8')
+ficheroNoticia2 = open('journal_news.txt','w', encoding='utf-8')
 #url = 'https://elpais.com/internacional/2022-08-06/taiwan-acusa-a-china-de-simular-un-ataque-a-la-isla.html/'
 #url = 'https://elpais.com/actualidad/newsletter-kiko-llaneras/2022-08-06/dime-cuanto-ganan-tus-amigos-y-te-dire-lo-pobre-o-rico-que-eres-tu.html/'
-#url = 'https://elpais.com/television/2022-08-10/jandro-mago-en-la-television-te-entierran-enseguida.html/'
-url = 'https://elpais.com/ciencia/2022-09-27/como-sabemos-si-el-impacto-de-la-sonda-dart-movio-realmente-el-asteroide.html'
+#url = 'https://elpais.com/educacion/2024-03-21/guia-para-no-perderse-en-los-nuevos-criterios-para-ser-profesor-titular-universitario-o-catedratico.html'
+url = 'https://elpais.com/ideas/2024-08-12/para-que-tenemos-amigos-de-segundo-rango.html'
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text,'lxml')
@@ -25,5 +25,4 @@ for quote in quotes:
 
 
 ficheroNoticia2.close()
-print("Fichero de texto creado!!")
  
